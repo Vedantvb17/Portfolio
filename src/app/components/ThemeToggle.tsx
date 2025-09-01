c727065601e3d62ea182fdea73ef720e876627ca
Venkatesh <venkatesh.gopinath@contentstack.com>
2025-09-01 18:24:14 +0530

    Update logic for theme toggle

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -9,30 +9,26 @@ import { useTheme } from 'next-themes';
 
 export default function ThemeToggle() {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
   useEffect(() => setMounted(true), []);
-
   if (!mounted) return null;
 
   return (
     <button
       className='p-2 rounded transition-colors duration-300 cursor-pointer'
-      style={{
-        backgroundColor: 'transparent',
-      }}
       onMouseEnter={(e) => {
-        e.currentTarget.style.backgroundColor = 'rgba(0,0,0,0.05)'; // light hover
-        if (theme === 'dark') {
-          e.currentTarget.style.backgroundColor = 'rgba(255,255,255,0.1)'; // dark hover
-        }
+        e.currentTarget.style.backgroundColor =
+          resolvedTheme === 'dark'
+            ? 'rgba(255,255,255,0.1)'
+            : 'rgba(0,0,0,0.05)';
       }}
       onMouseLeave={(e) => {
         e.currentTarget.style.backgroundColor = 'transparent';
       }}
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
-      <span className='text-xl'>{theme === 'dark' ? '🌞' : '🌙'}</span>
+      <span className='text-xl'>{resolvedTheme === 'dark' ? '🌞' : '🌙'}</span>
     </button>
   );
 }
